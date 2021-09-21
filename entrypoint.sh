d44efc4b97b329d8a52fed5d1c933be8950db117
fishsticks89 <61331006+fishsticks89@users.noreply.github.com>
2021-09-21 10:56:38 -0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 echo "Downloading PROS SDK"
 # Get VEX SDK and put it in ~/sdk
-python3 -m pip install --user pros-cli
+sudo python3 -m pip install pros-cli
 
 echo "Building Project"
 # Now make the makefile in the set path
